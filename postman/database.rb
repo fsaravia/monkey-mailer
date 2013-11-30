e288930d7e37403380a522e621e5892cb965ef3a
Federico Saravia Barrantes <fedesaravia@gmail.com>
2013-11-29 21:01:32 -0300

    Add datamapper logger configuration

--- postman/database.rb
+++ postman/database.rb
@@ -2,10 +2,13 @@ require_relative 'models/mail_queue.rb'
 
 module Postman
 
+  DataMapper::Logger.new(STDOUT, 'fatal')
+
   raise ArgumentError, 'One of the database names must be default' unless Postman.database.include?('default')
   Postman.database.each do |name, values|
     DataMapper.setup(name.to_sym, values)
   end
+
   DataMapper.finalize
 
   def self.find_mails(priority, quota)
