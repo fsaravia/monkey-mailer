6d7fb97c647e51212edf0021292df394123796c1
Federico Saravia Barrantes <fedesaravia@gmail.com>
2013-12-09 22:19:09 -0300

    Simplify configuration

--- lib/monkey-mailer/config.rb
+++ lib/monkey-mailer/config.rb
@@ -10,9 +10,8 @@ module MonkeyMailer
 
   class Configuration
 
-    attr_accessor :adapter, :mandril_api_key, :smtp_address, :smtp_port, :smtp_domain,
-    :smtp_user_name, :smtp_password, :smtp_authentication, :smtp_enable_starttls_auto,
-    :urgent_quota, :normal_quota, :low_quota, :normal_sleep, :low_sleep, :sleep, :options,
+    attr_accessor :adapter, :mandril_api_key, :smtp, :urgent_quota,
+    :normal_quota, :low_quota, :normal_sleep, :low_sleep, :sleep, :options,
     :loader
 
     @@defaults = {
@@ -43,29 +42,14 @@ module MonkeyMailer
     def initialize
       user_settings = File.exists?(@@config_file) ? YAML.load_file(@@config_file) : {}
       @@defaults.each_pair do |key, value|
-        if key === :options
-          @options = user_settings['options'] || value
-        else
-          value = user_settings[key.to_s] if user_settings.include?(key.to_s)
-          if value.is_a?(Hash)
-            value.each_pair {|sub_key, sub_value| self.send("#{key}_#{sub_key}=".to_sym, sub_value)}
-          else
-            self.send("#{key}=".to_sym,value)
-          end
-        end
+        value = user_settings[key.to_s] if user_settings.include?(key.to_s)
+        self.send("#{key}=".to_sym,value)
       end
     end
 
     def method_missing(*args)
       super unless options.include?(args[0])
       options[args[0]]
-    end
-
-    def smtp
-      options = {}
-      @@defaults[:smtp].each_key do |property|
-        options["smtp_#{property}".to_sym] = self.send("smtp_#{property}".to_sym)
-      end
     end
   end
 end
