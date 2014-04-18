5f79f713874cb98753fa67c518dd6e44ca678f9b
Federico Saravia Barrantes <fedesaravia@gmail.com>
2014-04-18 16:41:51 -0300

    Improve readability of dummy loader

--- lib/monkey-mailer/loaders/dummy.rb
+++ lib/monkey-mailer/loaders/dummy.rb
@@ -7,8 +7,14 @@ module MonkeyMailer::Loaders
     def find_emails(priority, quota)
       emails = []
       quota.times do
-        emails << MonkeyMailer::Email.new(:priority => priority, :to_email => 'info@example.com', :to_name => 'Example user',
-          :from_email => 'noreply@example.com', :from_name => 'No reply', :subject => 'Lorem ipsum', :body => '')
+        emails << MonkeyMailer::Email.new(
+          :priority => priority,
+          :to_email => 'info@example.com',
+          :to_name => 'Example user',
+          :from_email => 'noreply@example.com',
+          :from_name => 'No reply',
+          :subject => 'Lorem ipsum',
+          :body => '')
       end
       emails
     end
